52c1aa44daeb6ee01a243ad56555cdd1d1bde8f6
vikashub1 <106031152+vikashub1@users.noreply.github.com>
2022-08-19 14:41:30 -0400

    Update DeckTest.java

--- test/ca/sheridancollege/project/DeckTest.java
+++ test/ca/sheridancollege/project/DeckTest.java
@@ -33,25 +33,6 @@ public class DeckTest {
         Assert.assertEquals(expected, actual);
     }
 
-    //52 cards should be in enum ordinal, constructor test
-    /** @Test public void constructor_cardsPriorToShuffleInEnumOrdinal() {
-    Assert.assertEquals(testDeck.getCards().get(0), new Card(Rank.TWO, Suit.DIAMONDS));
-    Assert.assertEquals(testDeck.getCards().get(51), new Card(Rank.ACE, Suit.HEARTS));
-    }*/
-
-    //Cards should be the same before shuffling
-    /**@Test public void beforeShuffle() {
-    Assert.assertEquals(new Deck().getCards(), testDeck.getCards());
-    }*/
-
-    //Cards should be different after shuffling
-    //@Test
-
-    /**
-     * public void afterShuffle() {
-     * Assert.assertNotEquals(new Deck().shuffleDeck().getCards(), testDeck.getCards());
-     * }
-     */
 
     @Test
     public void getCardsCount_returnsCountOfCardsInNewDeck() {
@@ -87,20 +68,4 @@ public class DeckTest {
 
     }
 
-    /**@Test public void getCards_() {
-    List<Card> expected = new ArrayList<Card>(Arrays.asList(
-    new Card(Rank.TWO, Suit.DIAMONDS),
-    new Card(Rank.THREE, Suit.DIAMONDS),
-    new Card(Rank.FOUR, Suit.DIAMONDS),
-    new Card(Rank.FIVE, Suit.DIAMONDS), new Card(Rank.SIX, Suit.DIAMONDS), new Card(Rank.SEVEN, Suit.DIAMONDS), new Card(Rank.EIGHT, Suit.DIAMONDS), new Card(Rank.NINE, Suit.DIAMONDS), new Card(Rank.TEN, Suit.DIAMONDS), new Card(Rank.JACK, Suit.DIAMONDS), new Card(Rank.QUEEN, Suit.DIAMONDS),
-    new Card(Rank.KING, Suit.DIAMONDS), new Card(Rank.ACE, Suit.DIAMONDS), new Card(Rank.TWO, Suit.SPADES), new Card(Rank.THREE, Suit.SPADES), new Card(Rank.FOUR, Suit.SPADES), new Card(Rank.FIVE, Suit.SPADES),
-    new Card(Rank.SIX, Suit.SPADES), new Card(Rank.SEVEN, Suit.SPADES), new Card(Rank.EIGHT, Suit.SPADES), new Card(Rank.NINE, Suit.SPADES), new Card(Rank.TEN, Suit.SPADES), new Card(Rank.JACK, Suit.SPADES),
-    new Card(Rank.QUEEN, Suit.SPADES), new Card(Rank.KING, Suit.SPADES), new Card(Rank.ACE, Suit.SPADES), new Card(Rank.TWO, Suit.CLUBS), new Card(Rank.THREE, Suit.CLUBS), new Card(Rank.FOUR, Suit.CLUBS),
-    new Card(Rank.FIVE, Suit.CLUBS), new Card(Rank.SIX, Suit.CLUBS), new Card(Rank.SEVEN, Suit.CLUBS), new Card(Rank.EIGHT, Suit.CLUBS), new Card(Rank.NINE, Suit.CLUBS), new Card(Rank.TEN, Suit.CLUBS), new Card(Rank.JACK, Suit.CLUBS),
-    new Card(Rank.QUEEN, Suit.CLUBS), new Card(Rank.KING, Suit.CLUBS), new Card(Rank.ACE, Suit.CLUBS), new Card(Rank.TWO, Suit.HEARTS), new Card(Rank.THREE, Suit.HEARTS), new Card(Rank.FOUR, Suit.HEARTS), new Card(Rank.FIVE, Suit.HEARTS),
-    new Card(Rank.SIX, Suit.HEARTS), new Card(Rank.SEVEN, Suit.HEARTS), new Card(Rank.EIGHT, Suit.HEARTS), new Card(Rank.NINE, Suit.HEARTS), new Card(Rank.TEN, Suit.HEARTS), new Card(Rank.JACK, Suit.HEARTS), new Card(Rank.QUEEN, Suit.HEARTS),
-    new Card(Rank.KING, Suit.HEARTS), new Card(Rank.ACE, Suit.HEARTS)));
-    List<Card> actual = testDeck.getCards();
-    Assert.assertTrue(testDeck.equals(expected));
-    }*/
 }
